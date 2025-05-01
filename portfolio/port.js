786b8e498bcbe9cb5d9909f606a7fd867e22f76c
kanhaiya-kumar-kt <231fa04f40@gmail.com>
2025-05-01 11:51:43 +0530

    Add files via upload

---
+++ portfolio/port.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for reaching out! I'll get back to you soon.");
+  this.reset();
+});
